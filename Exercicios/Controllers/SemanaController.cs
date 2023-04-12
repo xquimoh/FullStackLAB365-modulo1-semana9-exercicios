988aee9575518678d5dafb67ffc8f1c441a9c4f6
xquimoh <michellemorikawa@hotmail.com>
2023-04-12 16:59:14 -0300

    SemanaController update

--- Exercicios/Controllers/SemanaController.cs
+++ Exercicios/Controllers/SemanaController.cs
@@ -9,42 +9,78 @@ public class SemanaController : Controller
 {
     private readonly SemanaContext _semanaContext;
 
+    // Construtor com parâmetro (injetado)
     public SemanaController(SemanaContext semanaContext)
     {
         _semanaContext = semanaContext;
     }
 
     [HttpGet]
-    public ActionResult Get()
+    public ActionResult<List<SemanaModel>> GetTodos()
     {
-        return Ok();
+        var listaSemanaModel = _semanaContext.Semana;
+        List<SemanaModel> listaGetTodos = new List<SemanaModel>();
+
+        foreach (var item in listaSemanaModel)
+        {
+            var semanaGetTodos = new SemanaModel();
+            semanaGetTodos.Id = item.Id;
+            semanaGetTodos.DataSemana = item.DataSemana;
+            semanaGetTodos.Conteudo = item.Conteudo;
+            semanaGetTodos.AplicandoConteudo = item.AplicandoConteudo;
+
+            listaGetTodos.Add(semanaGetTodos);
+        }
+
+        return Ok(listaGetTodos);
     }
     
     [HttpGet]
     [Route("{id}")]
-    public ActionResult Get([FromRoute] int id)
+    public ActionResult GetPorId([FromRoute] int id)
     {
-        return Ok();
+        SemanaModel _semanaModel = _semanaContext.Semana.Find(id);
+
+        if (_semanaModel != null)
+        {
+            return Ok(_semanaModel);
+        }
+
+        return BadRequest("Esse ID não existe :(");
     }
 
     [HttpPost]
     public ActionResult Post([FromBody] SemanaModel semanaModel)
     {
-        if (semanaModel.Id > 0)
+        if (semanaModel.Id >= 0)
         {
-            return Ok();
+            SemanaModel _semanaModel = new SemanaModel();
+            _semanaModel.DataSemana = semanaModel.DataSemana;
+            _semanaModel.Conteudo = semanaModel.Conteudo;
+            _semanaModel.AplicandoConteudo = semanaModel.AplicandoConteudo;
+
+            _semanaContext.Add(_semanaModel);
+            _semanaContext.SaveChanges();
+            return Ok(_semanaModel);
         }
 
         return BadRequest("O ID precisa ser maior que zero");
     }
 
     [HttpPut]
-    [Route("{id}")]
-    public ActionResult Put([FromBody] SemanaModel semanaModel, [FromRoute] int id)
+    public ActionResult Put([FromBody] SemanaModel semanaModel)
     {
-        if (semanaModel.Id == id)
+        SemanaModel _semanaModel = _semanaContext.Semana.Find(semanaModel.Id);
+
+        if (_semanaModel.Id != null)
         {
-            return Ok();
+            _semanaModel.DataSemana = semanaModel.DataSemana;
+            _semanaModel.Conteudo = semanaModel.Conteudo;
+            _semanaModel.AplicandoConteudo = semanaModel.AplicandoConteudo;
+
+            _semanaContext.Attach(_semanaModel);
+            _semanaContext.SaveChanges();
+            return Ok(semanaModel);
         }
 
         return BadRequest("Esse ID não existe :(");
@@ -54,7 +90,15 @@ public class SemanaController : Controller
     [Route("{id}")]
     public ActionResult Delete([FromRoute] int id)
     {
+        SemanaModel _semanaModel = _semanaContext.Semana.Find(id);
+
+        if (_semanaModel != null)
+        {
+            _semanaContext.Remove(_semanaModel);
+            _semanaContext.SaveChanges();
+            return Ok();
+        }
+
         return BadRequest("Esse ID não existe :(");
-    }
-     
+    }   
 }
